ebb7c1297f2002ba8e31b59534e96c2ce2c0572b
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-10-15 23:22:29 +0800

    Potentially correct calibration found

--- ph-calibration/ph-calibration.ino
+++ ph-calibration/ph-calibration.ino
@@ -11,23 +11,29 @@ void setup()
     Serial.println("cimpleo pH Sense");
 }
 
-float ph (float voltage) {
-    return 7 + ((2.5 - voltage) / 0.18);
+// Calibrate your sensor's readings
+float ph(float voltage)
+{
+    // Calculate the slope and offset based on the new calibration values
+    float slope = (7.0 - 2.7) / (2.7 - 2.86);
+    float offset = 7.0 - slope * 2.7;
+
+    return slope * voltage + offset;
 }
 
 void loop()
 {
-    int measurings=0;
+    int measurings = 0;
 
     for (int i = 0; i < samples; i++)
     {
         measurings += analogRead(pHSense);
         delay(10);
-
     }
 
-    float voltage = 5 / adc_resolution * measurings/samples;
-    Serial.print("pH= ");
-    Serial.println(ph(voltage));
-    delay(3000);
+    float voltage = 5.0 / adc_resolution * measurings / samples;
+    Serial.print(voltage);
+    Serial.print("V ");
+    Serial.print("pH = ");
+    Serial.println(ph(voltage), 2); // Display pH with two decimal places
 }
